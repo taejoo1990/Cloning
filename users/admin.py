22a47e88b18f144437d2edafcfec47327caa93e6
TED <taejoo1990@gmail.com>
2021-09-15 10:29:59 -0700

    Organization of UserModel :)

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,54 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models  # users\models.py
 
 # Register your models here.
-# sudo lsof -t -i tcp:8000 | xargs kill -9
+
+# admin.site.register(models.User, CustomUserAdmin)
+# !! i can code using either decorator(@admin.register) or class(admin.site.register(models.User, CustomUserAdmin))
 
 # registrate user's model
+# UserAdmin(django.contrib.auth.admin) provide basic form of display
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+    """Custon User Admin"""
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "super_host",
+                )
+            },
+        ),
+    )
+
+
+"""class CustomUserAdmin(admin.ModelAdmin) 
+using admin.ModelAdmin let me custormize list_display
+
+
+    CUSTOM USER ADMIN
+
+    list_display = (
+        "username",
+        "gender",
+        "email",
+        "language",
+        "currency",
+        "super_host",
+    )
+
+    list_filter = (
+        "super_host",
+        "gender",
+        "language",
+        "currency",
+    )"""
